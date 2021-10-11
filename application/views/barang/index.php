65144d7246236746baade2b97ca50aa4eb32aeff
fida meilinda effendi <fmeilinda18@gmail.com>
2021-10-11 16:12:14 +0700

    update project awal

--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -4,15 +4,31 @@
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> 
 </div> 
 <?php endif; ?>
+
+<?php if (empty($barang)):?> 
+  <div class="alert alert-danger" role="alert"> 
+    Data Barang Ditak ditemukan... 
+  </div> 
+  <?php endif; ?>
 <div class="container">
     <div class="row-mt-3">
         <div class="colmd-6">
             <a href="http://localhost/latihancodeigniter3/penjualan/barang/tambah/" class="btn btn-primary">Tambah Data Barang </a>
         </div>
 </div>
+<div class="row mt-3"> 
+  <div class="col-md-6"> 
+    <form action="" method="post"> 
+      <div class="input-group"> 
+        <input type="text" class="form-control" placeholder="Cari Data Barang..." name="keyword"> 
+        <button class="btn btn-primary" type="submit" >Cari</button> 
+      </div> 
+  </div>
+</div>
+    </form>
     <table class="table">
-    <thead>
-    <tr>
+       <thead>
+       <tr>
     <th scope="col">Kode Barang</th>
     <th scope="col">Nama Barang</th>
     <th scope="col">Harga</th>
