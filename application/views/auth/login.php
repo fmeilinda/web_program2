12abff4d13496603964df95cd5c7b088f046e4e6
fida meilinda effendi <fmeilinda18@gmail.com>
2021-11-19 14:12:25 +0700

    terbaru

---
+++ application/views/auth/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title><?= $judul; ?></title>
+    <div class="container">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+            <div class="col-lg-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <br>
+                        <br>
+                        <br><br>
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1><font color="black">Welcome Back!</font></h1>
+                                    </div>
+                                    <form class="user" method="post" action=<?= base_url('auth'); ?>>
+                                        <div class="form-group"> <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?= set_value('email'); ?>">
+                                            <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?> </div>
+                                        <div class="form-group"> <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="password"> </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block"> Login </button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center"> <a class="small" href="forgot-password.html">Forgot Password?</a> </div>
+                                    <div class="text-center"> <a class="small" href="http://localhost/latihancodeigniter3/penjualan/auth/registration/">Create an Account!</a> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</head>
---
+++ application/views/templates/auth_footer.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap core JavaScript-->
+<script src="http://localhost/latihancodeigniter3/penjualan/assets/vendor/jquery/jquery.min.js"></script>
+<script src="http://localhost/latihancodeigniter3/penjualan/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> <!-- Core plugin JavaScript-->
+<script src="http://localhost/latihancodeigniter3/penjualan/assets/vendor/jquery-easing/jquery.easing.min.js"></script> <!-- Custom scripts for all pages-->
+<script src="http://localhost/latihancodeigniter3/penjualan/assets/js/sb-admin-2.min.js"></script>
+</body>
+
+</html>
